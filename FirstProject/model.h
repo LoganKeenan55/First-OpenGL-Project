d5a446f6aa76bdb43107ec9f8eee829af00af083
Logan <logankeenan55@gmail.com>
2026-04-27 15:14:39 -0400

    Added support for indices and vertices in model class

--- FirstProject/model.h
+++ FirstProject/model.h
@@ -21,40 +21,19 @@ private:
 	std::vector<float> getFloats(json accessor);
 	std::vector<GLuint> getIndices(json accessor);
 
+	std::vector<Vertex> assembleVertices(
+		std::vector<glm::vec3> positions,
+		std::vector<glm::vec3> normals,
+		std::vector<glm::vec2> texUvs
+	);
+
 	std::vector<glm::vec2> groupFloatsVec2(std::vector<float> floatVec);
 	std::vector<glm::vec3> groupFloatsVec3(std::vector<float> floatVec);
 	std::vector<glm::vec4> groupFloatsVec4(std::vector<float> floatVec);
 
 };
 
-std::vector<float> Model::getFloats(json accessor) {
-	std::vector<float> floatVec;
 
-	unsigned int buffViewInd = accessor.value("bufferView", 1);
-	unsigned int count = accessor["count"];
-	unsigned int accByteOffset = accessor.value("byteOffset", 0);
-	std::string type = accessor["type"];
-	
-	json bufferView = JSON["bufferViews"][buffViewInd];
-	unsigned int byteOffset = bufferView["byteOffset"];
-
-	unsigned int numPerVert;
-	if (type == "SCALAR") numPerVert = 1;
-	else if (type == "VEC2") numPerVert = 2;
-	else if (type == "VEC3") numPerVert = 3;
-	else if (type == "VEC4") numPerVert = 4;
-	else throw std::invalid_argument("Type is invalid");
-
-	unsigned int beginningOfData = byteOffset + accByteOffset;
-	unsigned int lengthOfData = count * 4 * numPerVert;
-	for (unsigned int i = beginningOfData; i < beginningOfData + lengthOfData;i) {
-		
-
-	}
-
-
-}	
-	
 
 
 
